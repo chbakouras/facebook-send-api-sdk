cd6634adb7749c43e357e1f0cda394e8a1466f49
Chrisostomos Bakouras <chbakouras@gmail.com>
2018-02-14 02:40:42 +0200

    wip

--- src/model/component/button/ShareButton.js
+++ src/model/component/button/ShareButton.js
@@ -6,17 +6,19 @@ const shareButton = function (element) {
     const shareButtonObj = {};
 
     shareButtonObj.type = constants.ButtonType.ELEMENT_SHARE;
-    shareButtonObj.share_contents = {
-        attachment: {
-            type: constants.AttachmentType.TEMPLATE,
-            payload: {
-                template_type: constants.TemplateType.GENERIC,
-                elements: [
-                    element
-                ]
+    if (element) {
+        shareButtonObj.share_contents = {
+            attachment: {
+                type: constants.AttachmentType.TEMPLATE,
+                payload: {
+                    template_type: constants.TemplateType.GENERIC,
+                    elements: [
+                        element
+                    ]
+                }
             }
-        }
-    };
+        };
+    }
 
     return shareButtonObj;
 };
